8df0036d2d77e6bcc6f90f31c45d5fe10987c100
PunyaKeerthi <72212683+dileepSingh8962@users.noreply.github.com>
2020-12-16 23:53:37 +0530

    updated setup.py and created function

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Built-in',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='',
+    author='Punya Keerthi',
+    author_email='punyakeerthi39@gmail.com',
+    license='unlicense',
+    packages=['Built-in'],
+    zip_safe=False
+)
